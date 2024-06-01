f4292dc46e9583fbbdc292c8cd9ea495ff8436d7
Yash Sen <132945201+yaashhhh13@users.noreply.github.com>
2024-05-31 20:48:42 -0400

    database online

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require("passport");
+var expressSession = require("express-session")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var expressSession = require("express-session")
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ const commentModel = require("./comment.js");
 
 passport.use(new localStrategy(users.authenticate()));
 
-mongoose.connect("mongodb+srv://yashsen1007:ufzOGtjzwCOdjfFh@cluster0.dqlc5ja.mongodb.net/?retryWrites=true&w=majority").then(function(result){
+mongoose.connect("mongodb+srv://yashsen1007:bso7MeiE0TH1xfVA@instaclonedata.rb1lp7d.mongodb.net/?retryWrites=true&w=majority&appName=InstaCloneData").then(function(result){
   console.log("connected to database")
 }).catch(function(err){
   console.log(err)
