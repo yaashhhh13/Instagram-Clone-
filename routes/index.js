61ffb69532934a314a83052795d6af4267b6e65c
Yash Sen <132945201+yaashhhh13@users.noreply.github.com>
2024-08-11 09:58:59 -0400

    new ui

--- routes/index.js
+++ routes/index.js
@@ -12,390 +12,410 @@ const commentModel = require("./comment.js");
 
 passport.use(new localStrategy(users.authenticate()));
 
-mongoose.connect("mongodb+srv://yashsen1007:bso7MeiE0TH1xfVA@instaclonedata.rb1lp7d.mongodb.net/?retryWrites=true&w=majority&appName=InstaCloneData").then(function(result){
+mongoose.connect("mongodb+srv://yashsen1007:bso7MeiE0TH1xfVA@instaclonedata.rb1lp7d.mongodb.net/?retryWrites=true&w=majority&appName=InstaCloneData").then(function (result) {
   console.log("connected to database")
-}).catch(function(err){
+}).catch(function (err) {
   console.log(err)
 })
 
 try {
-  
-/* GET home page. */
-router.get('/', isLoggedIn ,async function(req, res, next) {
-  var allUsers = await users.find()
-  // res.render('index', { allUsers : allUsers });
-  res.send(allUsers)
-});
 
-router.get('/register', function(req, res, next) {
-  res.render('register');
-});
+  /* GET home page. */
+  router.get('/', isLoggedIn, async function (req, res, next) {
+    var allUsers = await users.find()
+    // res.render('index', { allUsers : allUsers });
+    res.send(allUsers)
+  });
 
-router.post("/register", function(req, res, next){
-  var newUserData = {
-    username: req.body.username,
-    profilePic: req.body.profilePic,
-    fullName: req.body.fullName,
-    bio: req.body.bio
-  };
+  router.get('/register', function (req, res, next) {
+    res.render('register');
+  });
 
-  users.register(newUserData, req.body.password)
-  .then(function(){
-    passport.authenticate("local")(req,res, function(){
-      res.redirect("/profile");
+  router.post("/register", function (req, res, next) {
+    var newUserData = {
+      username: req.body.username,
+      profilePic: req.body.profilePic,
+      fullName: req.body.fullName,
+      bio: req.body.bio
+    };
+
+    users.register(newUserData, req.body.password)
+      .then(function () {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/profile");
+        })
+      })
+  })
+
+
+  router.get('/login', function (req, res, next) {
+    res.render('login');
+  });
+
+  router.post("/login", passport.authenticate("local", {
+    successRedirect: "/feed",
+    failureRedirect: "/login"
+  }), function (req, res) { })
+
+
+  router.get("/logout", function (req, res) {
+    req.logOut(function (err) {
+      if (err) { return next(err) }
+      else {
+        res.redirect("/");
+      }
     })
   })
-})
 
-
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-
-router.post("/login", passport.authenticate("local",{
-  successRedirect: "/feed",
-  failureRedirect: "/login"
-}), function(req, res){})
-
-
-router.get("/logout", function(req, res){
-  req.logOut(function(err){
-    if(err) {return next(err)}
-    else {
-      res.redirect("/");
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
     }
-  })
-})
-
-function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()){
-    return next()
-  }
-  else {
-    res.redirect("/login");
-  }
-}
-
-
-router.get("/feed",isLoggedIn ,async (req, res, next) => {
-  
-  var allPosts = await postModel.find().populate('user').populate('comments').populate({path: 'comments', populate:('user')})
-
-  var allStories = await storyModel.find().populate('user');
-
-  var currentUser = await users.findOne({
-    username: req.session.passport.user
-  })
-
-  // console.log(allPosts)
-
-  res.render("feed", {allPosts,currentUser ,user: req.user, allStories});
-})
-
-
-router.get("/createPost", function(req, res, next){
-  res.render("createPost")
-})
-
-
-router.post("/createPost", isLoggedIn, async (req, res, next) =>{
-
-  const allUsers = await users.find()
-
-  const allPosts = await postModel.find().populate('user')
-
-  var newPost = new postModel({
-    media: req.body.media,
-    caption: req.body.caption,
-    mediaType: 'image',
-    user: req.user._id
-  })
-
-  await newPost.save()
-
-  res.redirect("/feed");
-})
-
-
-router.get("/like/:postId",isLoggedIn , async function(req, res, next){
-  var currentPost = await postModel.findById(req.params.postId);
-
-  var isAlreadyLiked = currentPost.likes.includes(req.user._id);
-
-  if(isAlreadyLiked){
-    currentPost.likes.pull(req.user._id);
-  }
-  else{
-    currentPost.likes.push(req.user._id)
+    else {
+      res.redirect("/login");
+    }
   }
 
-  await currentPost.save();
 
-  res.redirect("back");
-})
+  router.get("/feed", isLoggedIn, async (req, res, next) => {
 
+    var AllUsers = await users.find();
 
-router.get("/follow/:userId", isLoggedIn,async function(req, res, next){
-  var currentUser = await users.findById(req.user._id);
+    var allPosts = await postModel.find().populate('user').populate('comments').populate({ path: 'comments', populate: ('user') })
 
-  var oppositeUser = await users.findById(req.params.userId);
+    var allStories = await storyModel.find().populate('user');
 
-  var isAlreadyFollowed = await oppositeUser.follower.includes(currentUser._id);
-
-  if(isAlreadyFollowed){
-    oppositeUser.follower.pull(currentUser._id);
-    currentUser.following.pull(oppositeUser._id);
-    // res.json({status: "unfollowed"})
-  }
-  else{
-    oppositeUser.follower.push(currentUser._id);
-    currentUser.following.push(oppositeUser._id);
-    // res.json({status: "followed"})
-  }
-
-  await currentUser.save();
-  await oppositeUser.save();
-
-  res.redirect("/profile");
-})
-
-
-router.post("/createStory",isLoggedIn ,async function(req, res, next){
-  var newStory = new storyModel({
-    media: req.body.media,
-    caption: req.body.caption,
-    user: req.user._id
-  })
-
-  await newStory.save();
-
-  res.redirect("back")
-})
-
-
-router.get("/profile", isLoggedIn ,async function(req, res, next){
-
-  var currentUser = await users.findOne({
-    username: req.session.passport.user
-  })
-
-  var allPosts = await postModel.find().populate('user');
-
-  // console.log(currentUser);
-  // console.log(allPosts)
-
-  res.render("profile", {user: currentUser, posts: allPosts});
-})
-
-router.get("/profile/:id", isLoggedIn,async function(req, res, next){
-  var userDetails = await users.findOne({
-    _id: req.params.id
-  })
-
-  var allPosts = await postModel.find().populate('user');
-
-  res.render("profile", {user: userDetails, posts:allPosts})
-})
-
-router.get("/upload", function(req, res, next){
-  res.render("upload");
-})
-
-router.post("/addComment/:postId",isLoggedIn ,async function(req, res, next){
-  var currentPost = await postModel.findOne({
-    _id:req.params.postId
-  })
-
-  var loggedInUser = await users.findOne({
-    username: req.session.passport.user
-  })
-
-  var newComment = await commentModel.create({
-    text : req.body.comment,
-    user: loggedInUser._id,
-    post: currentPost._id
-  })
-
-  currentPost.comments.push(newComment._id);
-
-  await currentPost.save();
-
-  res.redirect("back");
-})
-
-
-router.get("/delete/:commentId",async function(req, res, next){
-
-  var loggedInUser = await users.findOne({
-    username: req.session.passport.user
-  })
-
-
-  var currentComment = await commentModel.findOne({
-    _id: req.params.commentId
-  }).populate('user');
-
-  var currentPost = await postModel.findOne({
-    _id: currentComment.post
-  }).populate('user')
-
-  if(currentComment.user.username == loggedInUser.username || currentPost.user.username == loggedInUser.username){
-    await commentModel.findOneAndDelete({
-      _id: currentComment._id
+    var currentUser = await users.findOne({
+      username: req.session.passport.user
     })
-  }
-  else{
-    res.json({
-      data: "you cant delete this comment as this is not your post"
-    })
-    return
-  }
 
-  await currentPost.comments.pull(currentComment._id);
+    // console.log(allPosts)
 
-  currentPost.save();
-
-  res.redirect("back");
-})
-
-router.get("/likeStory/:storyId",isLoggedIn,async function(req, res, next){
-  var CurrentStory = await storyModel.findOne({
-    _id: req.params.storyId
+    res.render("feed", { allPosts, AllUsers, currentUser, user: req.user, allStories });
   })
 
-  var StoryAlreadyLiked = await CurrentStory.likes.includes(req.user._id);
 
-  if(StoryAlreadyLiked){
-    CurrentStory.likes.pull(req.user._id);
-    res.json({
-      status: "liked"
+  router.get("/createPost", function (req, res, next) {
+    res.render("createPost")
+  })
+
+
+  router.post("/createPost", isLoggedIn, async (req, res, next) => {
+
+    const allUsers = await users.find()
+
+    const allPosts = await postModel.find().populate('user')
+
+    var newPost = new postModel({
+      media: req.body.media,
+      caption: req.body.caption,
+      mediaType: 'image',
+      user: req.user._id
     })
-  }
-  else{
-    CurrentStory.likes.push(req.user._id);
-    res.json({
-      status: "disliked"
+
+    await newPost.save()
+
+    res.redirect("/feed");
+  })
+
+
+  router.get("/like/:postId", isLoggedIn, async function (req, res, next) {
+    var currentPost = await postModel.findById(req.params.postId);
+
+    var isAlreadyLiked = currentPost.likes.includes(req.user._id);
+
+
+    if (isAlreadyLiked) {
+      currentPost.likes.pull(req.user._id);
+      res.json({
+        status: "liked"
+      })
+    }
+    else {
+      currentPost.likes.push(req.user._id);
+      res.json({
+        status: "disliked"
+      })
+    }
+
+    await currentPost.save();
+    
+  })
+
+
+  router.get("/follow/:userId", isLoggedIn, async function (req, res, next) {
+    var currentUser = await users.findById(req.user._id);
+
+    var oppositeUser = await users.findById(req.params.userId);
+
+    var isAlreadyFollowed = await oppositeUser.follower.includes(currentUser._id);
+
+    if (isAlreadyFollowed) {
+      oppositeUser.follower.pull(currentUser._id);
+      currentUser.following.pull(oppositeUser._id);
+      // res.json({status: "unfollowed"})
+    }
+    else {
+      oppositeUser.follower.push(currentUser._id);
+      currentUser.following.push(oppositeUser._id);
+      // res.json({status: "followed"})
+    }
+
+    await currentUser.save();
+    await oppositeUser.save();
+
+    res.redirect("/profile");
+  })
+
+
+  router.post("/createStory", isLoggedIn, async function (req, res, next) {
+    var newStory = new storyModel({
+      media: req.body.media,
+      caption: req.body.caption,
+      user: req.user._id
     })
-  }
 
-  await CurrentStory.save();
+    await newStory.save();
 
-  // res.redirect("back");
-
-  // res.json({
-  //   status: "liked"
-  // })
-})
+    res.redirect("/feed")
+  })
 
 
-router.get("/deleteStory/:storyId",isLoggedIn,async function(req,res,next){
-  var CurrentStory = await storyModel.findOne({_id: req.params.storyId}).populate('user')
+  router.get("/profile", isLoggedIn, async function (req, res, next) {
 
-  var loggedInUser = await users.findOne({username: req.session.passport.user});
+    var currentUser = await users.findOne({
+      username: req.session.passport.user
+    })
 
-  if(CurrentStory.user.username == loggedInUser.username){
-    await storyModel.findOneAndDelete({
+    var allPosts = await postModel.find().populate('user');
+
+    // console.log(currentUser);
+    // console.log(allPosts)
+
+    res.render("profile", { user: currentUser, posts: allPosts });
+  })
+
+  router.get("/profile/:id", isLoggedIn, async function (req, res, next) {
+    var userDetails = await users.findOne({
+      _id: req.params.id
+    })
+
+    var allPosts = await postModel.find().populate('user');
+
+    res.render("profile", { user: userDetails, posts: allPosts })
+  })
+
+  router.get("/upload", function (req, res, next) {
+    res.render("upload");
+  })
+
+  router.post("/addComment/:postId", isLoggedIn, async function (req, res, next) {
+    var currentPost = await postModel.findOne({
+      _id: req.params.postId
+    })
+
+    var loggedInUser = await users.findOne({
+      username: req.session.passport.user
+    })
+
+    var newComment = await commentModel.create({
+      text: req.body.comment,
+      user: loggedInUser._id,
+      post: currentPost._id
+    })
+
+    currentPost.comments.push(newComment._id);
+
+    await currentPost.save();
+
+    res.redirect("back");
+  })
+
+
+  router.get("/delete/:commentId", async function (req, res, next) {
+
+    var loggedInUser = await users.findOne({
+      username: req.session.passport.user
+    })
+
+
+    var currentComment = await commentModel.findOne({
+      _id: req.params.commentId
+    }).populate('user');
+
+    var currentPost = await postModel.findOne({
+      _id: currentComment.post
+    }).populate('user')
+
+    if (currentComment.user.username == loggedInUser.username || currentPost.user.username == loggedInUser.username) {
+      await commentModel.findOneAndDelete({
+        _id: currentComment._id
+      })
+    }
+    else {
+      res.json({
+        data: "you cant delete this comment as this is not your post"
+      })
+      return
+    }
+
+    await currentPost.comments.pull(currentComment._id);
+
+    currentPost.save();
+
+    res.redirect("back");
+  })
+
+  router.get("/likeStory/:storyId", isLoggedIn, async function (req, res, next) {
+    var CurrentStory = await storyModel.findOne({
       _id: req.params.storyId
     })
-  }
-  else{
-    // res.send("this story is not uploaded by you")
-    res.redirect("/storydelete")
-    return
-  }
 
-  res.redirect("/feed");
-})
+    var StoryAlreadyLiked = await CurrentStory.likes.includes(req.user._id);
 
+    if (StoryAlreadyLiked) {
+      CurrentStory.likes.pull(req.user._id);
+      res.json({
+        status: "liked"
+      })
+    }
+    else {
+      CurrentStory.likes.push(req.user._id);
+      res.json({
+        status: "disliked"
+      })
+    }
 
-router.get("/storydelete", isLoggedIn, function(req, res, next){
-  res.render("storydelete");
-})
+    await CurrentStory.save();
 
-router.get("/Search", isLoggedIn,async function(req, res, next){
-  var AllUsers = await users.find();
+    // res.redirect("back");
 
-  var Allposts = await postModel.find().populate('user');
-
-  var currentUser = await users.findOne({
-    username: req.session.passport.user
-  })
-  
-  res.render("Search", {users: AllUsers, posts: Allposts, currentUser});
-})
-
-
-router.post("/searchUser", isLoggedIn,async function(req, res, next){
-  // console.log(req.body.data);
-  var searchedUser = await users.find({
-    username: {$regex: req.body.data}
-  })
-  res.json({searchedUser});
-})
-
-
-router.get("/savepost/:saveId", isLoggedIn,async function(req, res, next){
-
-  var loggedInUser = await users.findOne({
-    username: req.session.passport.user
+    // res.json({
+    //   status: "liked"
+    // })
   })
 
-  console.log(loggedInUser);
 
-  var currentPost = await postModel.findOne({
-    _id: req.params.saveId
-  }).populate('user')
-  
-  // console.log(loggedInUser.user.savedPosts)
+  router.get("/deleteStory/:storyId", isLoggedIn, async function (req, res, next) {
+    var CurrentStory = await storyModel.findOne({ _id: req.params.storyId }).populate('user')
 
-  await loggedInUser.savedPosts.push(currentPost._id);
+    var loggedInUser = await users.findOne({ username: req.session.passport.user });
 
-  await loggedInUser.save();
+    if (CurrentStory.user.username == loggedInUser.username) {
+      await storyModel.findOneAndDelete({
+        _id: req.params.storyId
+      })
+    }
+    else {
+      // res.send("this story is not uploaded by you")
+      res.redirect("/storydelete")
+      return
+    }
 
-  // console.log(loggedInUser);
-
-  await currentPost.save();
-
-  res.redirect("/savedPosts");
-
-})
-
-router.get("/savedPosts", isLoggedIn,async function(req, res, next){
-  
-  var currentLoggedUser = await users.findOne({
-    username: req.session.passport.user
+    res.redirect("/feed");
   })
 
-  var allPosts = await postModel.find()
 
-  res.render("savedPosts", {user : currentLoggedUser, posts: allPosts, })
-})
-
-
-router.get("/editProfile/:userId", isLoggedIn,async function(req, res, next){
-  var EditUserDEtails = await users.findOne({
-    _id: req.params.userId
+  router.get("/storydelete", isLoggedIn, function (req, res, next) {
+    res.render("storydelete");
   })
 
-  res.render("editProfile", {user: EditUserDEtails});
-})
+  router.get("/Search", isLoggedIn, async function (req, res, next) {
+    var AllUsers = await users.find();
 
+    var Allposts = await postModel.find().populate('user');
 
-router.post("/updateProfileDetails/:id", isLoggedIn,async function(req, res, next){
-  var UpdateDetails = await users.findOneAndUpdate({
-    _id: req.params.id,
-    username:req.body.username,
-    fullName: req.body.fullName,
-    profilePic: req.body.profilePic,
-    bio: req.body.bio
+    var currentUser = await users.findOne({
+      username: req.session.passport.user
+    })
+
+    res.render("Search", { users: AllUsers, posts: Allposts, currentUser });
   })
 
-  await UpdateDetails.save();
 
-  res.redirect("/profile")
-})
+  router.post("/searchUser", isLoggedIn, async function (req, res, next) {
+    // console.log(req.body.data);
+    var searchedUser = await users.find({
+      username: { $regex: req.body.data }
+    })
+    res.json({ searchedUser });
+  })
 
-// Try & Catch Function Ends here
+
+  router.get("/savepost/:saveId", isLoggedIn, async function (req, res, next) {
+
+    var loggedInUser = await users.findOne({
+      username: req.session.passport.user
+    })
+
+
+    var currentPost = await postModel.findOne({
+      _id: req.params.saveId
+    }).populate('user')
+
+    // console.log(loggedInUser.user.savedPosts)
+
+    await loggedInUser.savedPosts.push(currentPost._id);
+
+    await loggedInUser.save();
+
+    // console.log(loggedInUser);
+
+    await currentPost.save();
+
+    res.redirect("/savedPosts");
+
+  })
+
+  router.get("/savedPosts", isLoggedIn, async function (req, res, next) {
+
+    var currentLoggedUser = await users.findOne({
+      username: req.session.passport.user
+    })
+
+    var allPosts = await postModel.find()
+
+    res.render("savedPosts", { user: currentLoggedUser, posts: allPosts, })
+  })
+
+
+  router.get("/editProfile/:userId", isLoggedIn, async function (req, res, next) {
+    var EditUserDEtails = await users.findOne({
+      _id: req.params.userId
+    })
+
+    res.render("editProfile", { user: EditUserDEtails });
+  })
+
+
+  router.post("/updateProfileDetails/:id", isLoggedIn, async function (req, res, next) {
+    var UpdateDetails = await users.findOneAndUpdate({
+      _id: req.params.id,
+      username: req.body.username,
+      fullName: req.body.fullName,
+      profilePic: req.body.profilePic,
+      bio: req.body.bio
+    })
+
+    await UpdateDetails.save();
+
+    res.redirect("/profile")
+  })
+
+
+  router.get("/fullStory", isLoggedIn, async (req, res, next) => {
+
+    var stories = await storyModel.find().populate('user');
+
+    res.render("fullStory", { stories: stories })
+  })
+
+
+  router.get("/*", (err, req, res, next) => {
+    res.render("error", { message: err.message })
+  })
+
+  // Try & Catch Function Ends here
 } catch (error) {
   console.log(error)
 }
